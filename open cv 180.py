c7ffe4c11ff30d17b92b4f383a0f3b7bba529347
Harirock15 <113888642+Harirock15@users.noreply.github.com>
2023-05-15 10:23:28 +0530

    Create open cv 180.py

---
+++ open cv 180.py
@@ -0,0 +1,18 @@
+# rotattion 180
+import cv2
+
+
+path = "C:/Users/Hari Prasad/Pictures/Screenshots/Screenshot_20230202_192521.png"
+
+
+src = cv2.imread(path)
+
+
+window_name = 'Image'
+
+
+image = cv2.rotate(src, cv2.ROTATE_180)
+
+
+cv2.imshow(window_name, image)
+cv2.waitKey(0)
